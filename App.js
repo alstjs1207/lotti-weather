4baf3396e907462042cc64be8c76e2daee4db98a
alstjs1207 <alstjs1207@naver.com>
2020-03-05 23:21:27 +0900

    Loading pages

--- App.js
+++ App.js
@@ -1,19 +1,7 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Loading from './Loading';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello!!</Text>
-    </View>
-  );
+  return <Loading />;
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
