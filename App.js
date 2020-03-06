89ca1579a5bb43f95fe668b1a2cb425b36287a7b
alstjs1207 <alstjs1207@naver.com>
2020-03-06 18:40:32 +0900

    Getting the Weather

--- App.js
+++ App.js
@@ -1,7 +1,41 @@
 import React from 'react';
+import {Alert} from "react-native";
 import Loading from './Loading';
+import * as Location from 'expo-location';
+import axios from 'axios';
 
-export default function App() {
-  return <Loading />;
+const API_KEY = '';
+
+export default class App extends React.Component {
+
+  state = {
+    isLoading : true
+  };
+
+  getWeather = async(latitude,longitude) => {
+    const {data} = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`);
+  }
+
+  getLocation = async () => {
+    try {
+      await Location.requestPermissionsAsync();
+      const {coords:{latitude, longitude}} = await Location.getCurrentPositionAsync();
+
+      // send to API and get Weather
+      this.getWeather(latitude, longitude);
+      this.setState({ isLoading: false });
+
+    } catch (error) {
+      Alert.alert("Can't find you.","So sad");
+    }
+
+  };
+  componentDidMount() {
+    this.getLocation();
+  }
+
+  render() {
+    const { isLoading } = this.state;
+    return (isLoading ? <Loading /> : null);
+  }
 }
-
--- Loading.js
+++ Loading.js
@@ -3,7 +3,7 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function Loading(){
     return (<View style={styles.container}>
-        <Text style={styles.text}> Getting the Lott Weather</Text>
+        <Text style={styles.text}> Getting the Lott Weather!!</Text>
     </View>);
 }
 
